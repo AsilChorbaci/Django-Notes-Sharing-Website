278e6bb103a6c723f1634067690f0cc8ded36856
AsilChorbaci <asil.chorbaci@gmail.com>
2021-03-31 02:52:14 +0300

    Added Note Image Gallery

--- notes/admin.py
+++ notes/admin.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
 
 # Register your models here.
-from notes.models import Category, Note
+from django.utils.html import format_html
+from notes.models import Category, Note, Images
 
-admin.site.register(Category)
-admin.site.register(Note)
+class NoteImageInline(admin.TabularInline):
+    model = Images
+    extra = 5
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status' , 'image_tag']
+    readonly_fields = ('image_tag',)
+    list_filter = ['status']
+
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'status','image_tag')
+    readonly_fields = ('image_tag',)
+    list_filter = ['status', 'category']
+    inlines = [NoteImageInline]
+    prepopulated_fields = {'slug': ('title',)}
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title', 'note']
+    #readonly_fields = ('image_tag',)
+
+admin.site.register(Category , CategoryAdmin)
+admin.site.register(Note , NoteAdmin)
+admin.site.register(Images)
--- notes/models.py
+++ notes/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib import admin
 
 # Create your models here.
+from django.utils.safestring import mark_safe
+
+
 class Category(models.Model):
     STATUS = (
         ('True', 'True'),
@@ -19,6 +22,11 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+
+    image_tag.short_description = 'Image'
 
 class Note(models.Model):
     STATUS = (
@@ -39,3 +47,21 @@ class Note(models.Model):
 
     def _str_(self):
         return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+
+    image_tag.short_description = 'Image'
+
+class Images(models.Model):
+    note = models.ForeignKey(Note, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, blank=True)
+    image = models.ImageField(blank=True, upload_to='images/')
+
+    def _str_(self):
+        return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+
+    image_tag.short_description = 'Image'
