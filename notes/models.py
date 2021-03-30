c87cdac8014ff2dda1b61379983cf1db8caccbb5
AsilChorbaci <asil.chorbaci@gmail.com>
2021-03-31 00:38:58 +0300

    Added TEMPS Folder And Added New Model Function

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
+class Category(models.Model):
+    STATUS = (
+        ('True', 'True'),
+        ('False', 'False'),
+    )
+    title = models.CharField(max_length=30)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    status = models.CharField(max_length=10, choices=STATUS)
+    slug = models.SlugField()
+    Parent = models.ForeignKey('self', blank=True, null=True, related_name='children',on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Note(models.Model):
+    STATUS = (
+        ('True', 'True'),
+        ('False', 'False'),
+    )
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # relation with Category table
+    title = models.CharField(max_length=200)
+    keywords = models.CharField(blank=True,max_length=255)
+    description = models.CharField(blank=True,max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    detail = models.TextField()
+    slug = models.SlugField(null=False, unique=True)
+    status = models.CharField(max_length=10, choices=STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def _str_(self):
+        return self.title
