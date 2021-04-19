f4744a4b9efac8b0596d323b9e3fd02c1bf18c13
AsilChorbaci <asil.chorbaci@gmail.com>
2021-04-20 02:49:53 +0300

    Template Setup

--- home/models.py
+++ home/models.py
@@ -1,2 +1,33 @@
+from ckeditor_uploader.fields import RichTextUploadingField
 from django.db import models
 
+class Setting(models.Model):
+    STATUS = (
+        ('True' , 'True'),
+        ('False' , 'False'),
+    )
+    title =models.CharField(max_length=200)
+    company = models.CharField(max_length=50)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    address = models.TextField(blank=True)
+    phone = models.CharField(blank=True, max_length=15)
+    fax = models.CharField(blank=True, max_length=15)
+    email = models.CharField(blank=True, max_length=50)
+    smtpserver = models.CharField(blank=True, max_length=50)
+    smtpemail = models.CharField(blank=True, max_length=50)
+    smtppassword = models.CharField(blank=True, max_length=10)
+    smtpport = models.CharField(blank=True, max_length=5)
+    icon = models.ImageField(blank=True, upload_to='images/')
+    facebook = models.CharField(blank=True, max_length=50)
+    instagram = models.CharField(blank=True, max_length=50)
+    twitter = models.CharField(blank=True, max_length=50)
+    aboutus = RichTextUploadingField(blank=True)
+    contact = RichTextUploadingField(blank=True)
+    references = RichTextUploadingField(blank=True)
+    status = models.CharField(max_length=10, choices=STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def _str_(self):
+        return self.title
