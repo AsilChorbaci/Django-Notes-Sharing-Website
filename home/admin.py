85dac62226fa28c8105c865df0f082ef01c1352c
AsilChorbaci <asil.chorbaci@gmail.com>
2021-05-08 02:57:10 +0300

    Added Contact Page

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from home.models import Setting
+from home.models import Setting, ContactFormMessage
 # Register your models here.
 
 class SettingAdmin(admin.ModelAdmin):
     list_display = ['title', 'company', 'update_at' ,'status']
 
+class ContactFormMessageAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'subject','message','phone','status']
+    list_filter = ['status']
+
 admin.site.register(Setting , SettingAdmin)
+admin.site.register(ContactFormMessage,ContactFormMessageAdmin)
--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.template import Template,Context
 
-from home.models import Setting
+from home.models import Setting, ContactForm, ContactFormMessage
+from notes.models import Category
 
 
 def index(request):
@@ -18,6 +20,22 @@ def about(request):
     return render(request, 'about.html', context)
 
 def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage()
+            data.name = form.cleaned_data['name']
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.phone = form.cleaned_data['phone']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            messages.success(request, 'Your Message Has Been Sent, Thank You')
+            return HttpResponseRedirect('/contact')
+
     setting = Setting.objects.get(pk=1)
-    context = {'setting': setting}
+    form = ContactForm()
+    category = Category.objects.all()
+    context = {'setting': setting, 'form': form, 'category': category}
     return render(request, 'contact.html', context)
