43ac52f615198daad7fbd7a4092e6d455f0c536e
AsilChorbaci <asil.chorbaci@gmail.com>
2021-06-06 17:12:57 +0300

    Added Update Profile

--- user/views.py
+++ user/views.py
@@ -1,11 +1,11 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from notes.models import Category, Note
-from user.forms import SignUpForm
+from user.forms import SignUpForm, UserUpdateForm, ProfileUpdateForm
 from user.models import UserProfile
 
 
@@ -69,3 +69,28 @@ def register_form(request):
 def logout_fun(request):
     logout(request)
     return HttpResponseRedirect('/')
+
+
+
+def user_update(request):
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.userprofile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Profilin Güncellendi')
+            return HttpResponseRedirect('/user')
+    else:
+        category = Category.objects.all()
+        current_user = request.user
+        profile = UserProfile.objects.get(pk=current_user.id)
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.userprofile)
+        context = {
+            'category': category,
+            'user_form': user_form,
+            'profile_form': profile_form,
+            'profile':profile
+        }
+        return render(request, 'user_update.html', context)
