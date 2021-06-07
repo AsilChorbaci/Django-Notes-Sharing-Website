ce590caeb9722e7809f715563aac3d59852522a2
AsilChorbaci <asil.chorbaci@gmail.com>
2021-06-07 03:00:30 +0300

    Added Comment List And Delete

--- user/urls.py
+++ user/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     #path('addcomment/<int:id>', views.addcomment,name='addcomment')
     path('update/', views.user_update, name='ser_update'),
     path('password/', views.change_password, name='change_password'),
+    path('comments/', views.user_comments, name='user_comments'),
+    path('deletecomment/<int:id>', views.deletecomment, name='deletecomment'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,11 +1,12 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import PasswordChangeForm
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 
 # Create your views here.
-from notes.models import Category, Note
+from notes.models import Category, Note, Comment
 from user.forms import SignUpForm, UserUpdateForm, ProfileUpdateForm
 from user.models import UserProfile
 
@@ -115,3 +116,23 @@ def change_password(request):
         return render(request,'change_password.html', {
             'form': form, 'category': category
         })
+
+
+@login_required(login_url='/login')
+def user_comments(request):
+    category = Category.objects.all()
+    current_user = request.user
+    comments = Comment.objects.filter(user_id=current_user.id)
+    context = {
+        'category': category,
+        'comments': comments,
+    }
+    return render(request, 'user_comment.html', context)
+
+
+@login_required(login_url='/login')
+def deletecomment(request, id):
+    current_user =request.user
+    Comment.objects.filter(id=id, user_id=current_user.id).delete()
+    messages.success(request, 'Comment Has Been Deleted')
+    return HttpResponseRedirect('/user/comments')
